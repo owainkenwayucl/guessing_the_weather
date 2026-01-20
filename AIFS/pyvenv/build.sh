a42fe30d8ab712f5c784c8d47e931948cdd286e0
Owain Kenway <o.kenway@ucl.ac.uk>
2026-01-20 16:19:44 +0000

    Order of arguments matters

--- AIFS/pyvenv/build.sh
+++ AIFS/pyvenv/build.sh
@@ -4,5 +4,5 @@ set -e
 
 pip install --upgrade -r prereqs.txt
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
-pip install -r --no-build-isolation requirements.txt
+pip install --no-build-isolation -r requirements.txt
 pip install -r image.txt
