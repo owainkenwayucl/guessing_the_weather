7a689e436821cf36fe99573c4c635b08348a2f9a
Owain Kenway <o.kenway@ucl.ac.uk>
2026-01-20 16:15:21 +0000

    Multi-step build process

---
+++ AIFS/pyvenv/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pip install --upgrade -r prereqs.txt
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+pip install -r --no-build-isolation requirements.txt
+pip install -r image.txt
