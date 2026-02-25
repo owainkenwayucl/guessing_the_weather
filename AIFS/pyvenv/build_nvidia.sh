26d361c93ef3d64b2cd417fb413e3ea3d31d3b65
Owain Kenway <o.kenway@ucl.ac.uk>
2026-02-25 15:08:34 +0000

    PAIN

--- AIFS/pyvenv/build_nvidia.sh
+++ AIFS/pyvenv/build_nvidia.sh
@@ -3,6 +3,6 @@
 set -e
 
 pip install --upgrade -r prereqs.txt
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
 pip install --no-build-isolation -r requirements.txt
 pip install -r image.txt
