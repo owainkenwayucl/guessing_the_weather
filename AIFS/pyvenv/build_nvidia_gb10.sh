ed86c671d928d0488e854500221726d0b49ea456
Owain Kenway <o.kenway@ucl.ac.uk>
2026-02-25 17:54:33 +0000

    version of script for hackathon

---
+++ AIFS/pyvenv/build_nvidia_gb10.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# On GB10 we have memory issues building Flash Attention
+export MAX_JOBS=4
+export NVCC_THREADS=2
+export CMAKE_BUILD_PARALLEL_LEVEL=4
+export FLASH_ATTENTION_FORCE_BUILD="TRUE"
+
+# Download wheel from ARC wheel shop
+wget https://wheelshop.arc-general.condenser.arc.ucl.ac.uk/flash_attn-2.8.3-cp312-cp312-linux_aarch64.whl
+
+pip install --upgrade -r prereqs.txt
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130
+pip install ./flash_attn-2.8.3-cp312-cp312-linux_aarch64.whl
+pip install --no-build-isolation -r requirements.txt
+pip install -r image.txt
