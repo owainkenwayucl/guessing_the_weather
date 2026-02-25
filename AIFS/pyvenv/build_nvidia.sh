1efdaa1cbd24d999caf5e0e692477b349579a9ba
Owain Kenway <o.kenway@ucl.ac.uk>
2026-02-25 15:54:32 +0000

    ARRRMMMM

--- AIFS/pyvenv/build_nvidia.sh
+++ AIFS/pyvenv/build_nvidia.sh
@@ -2,7 +2,13 @@
 
 set -e
 
+# On GB10 we have memory issues building Flash Attention
+export MAX_JOBS=4
+export NVCC_THREADS=2
+export CMAKE_BUILD_PARALLEL_LEVEL=4
+export FLASH_ATTENTION_FORCE_BUILD="TRUE"
+
 pip install --upgrade -r prereqs.txt
-pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu130
 pip install --no-build-isolation -r requirements.txt
 pip install -r image.txt
